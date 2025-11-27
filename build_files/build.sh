d8dc48a8b6531a830af35d895cadfe0a9369a067
nalch <nalch@abc.de>
2025-11-27 22:34:28 +0000

    feat: add ghostty via copr

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,17 +13,17 @@ set -ouex pipefail
 dnf5 install -y gh htop zsh sway waybar swaylock
 
 # Use a COPR Example:
-#
+curl -fsSL "https://copr.fedorainfracloud.org/coprs/scottames/ghostty/repo/fedora-${VERSION_ID}/scottames-ghostty-fedora-${VERSION_ID}.repo" | sudo tee /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:scottames:ghostty.repo > /dev/null
 dnf5 -y copr enable ublue-os/staging
+
 dnf5 -y install ghostty
+
 # Disable COPRs so they don't end up enabled on the final image:
 dnf5 -y copr disable ublue-os/staging
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-
-curl -fsSL "https://copr.fedorainfracloud.org/coprs/scottames/ghostty/repo/fedora-${VERSION_ID}/scottames-ghostty-fedora-${VERSION_ID}.repo" | sudo tee /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:scottames:ghostty.repo > /dev/null
 
 cp /01-nalch.just /usr/share/ublue-os/just/
 
